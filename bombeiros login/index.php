04b35ec939f0c9d47c9760e320563389bc79c64f
KalfelsWiggers <124676274+KalfelsWiggers@users.noreply.github.com>
2023-09-19 09:20:36 -0300

    Add files via upload

---
+++ bombeiros login/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Login</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="azul-escuro">
+        <img src="img/logo_grande.png" alt="logo bombeiros">
+        <div class="azul-claro">
+            <h1>LOGIN</h1>
+            <form action="">
+                <div class="form-column">
+                    <input type="text" name="" id="" placeholder="Digite seu email">
+                    <hr>
+                    <input type="password" name="" id="" placeholder="Digite sua senha">
+                    <hr>
+
+                </div>
+                <div class="form-row">
+                    <div class="checkbox">
+                        <input type="checkbox" name="" id="check">
+                        <label for="check">Lembre de mim</label>
+                    </div>
+                    <input type="submit" value="ENTRAR" class="btn">
+                </div>
+            </form>
+        </div>
+
+    </div>
+</body>
+</html>
