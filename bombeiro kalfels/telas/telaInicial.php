940e8a1bc3a49152fe7d40924da2225ef8bb1f39
KalfelsWiggers <124676274+KalfelsWiggers@users.noreply.github.com>
2023-09-21 12:26:42 -0300

    Add files via upload
    TERMINAR

---
+++ bombeiro kalfels/telas/telaInicial.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Inicial</title>
+    <link rel="stylesheet" href="../css/telaInicial.css">
+</head>
+<body>
+    <header>
+        <img src="../img/logo_grande.png" alt="logo noar">
+        <h1>Associação Humanitária de Busca Salvamento Aéreo e Resgate</h1>
+    </header>
+    <main>
+        <section>
+            <button> Nova Ocorrência </button>
+        </section>
+        <hr>
+        <section>
+            <button> Histórico de Ocorrências </button>
+        </section>
+    </main>
+</body>
+</html>
