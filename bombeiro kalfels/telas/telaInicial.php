40fcc90b26c5611f26879a25d1ccfeff82a2aeb0
KalfelsWiggers <124676274+KalfelsWiggers@users.noreply.github.com>
2023-10-04 08:15:53 -0300

    Add files via upload

--- bombeiro kalfels/telas/telaInicial.php
+++ bombeiro kalfels/telas/telaInicial.php
@@ -8,6 +8,11 @@
     <link rel="stylesheet" href="../css/telaInicial.css">
 </head>
 <body>
+    <nav class="hidden_nav">
+        <ul>
+            <li><a href="#">SAIR</a></li>
+        </ul>
+    </nav>
     <header>
         <div class="logo">
             <img src="../img/logo_grande.png" alt="logo noar">
@@ -15,7 +20,7 @@
             <h1 id="cellphone-only">AHBSAR</h1>
         </div>
         <div class="menu">
-            <div class="menu-burguer">aaa</div>
+            <div class="menu_burguer"></div>
         </div>
     </header>
     <main>
@@ -30,4 +35,15 @@
         </section>
     </main>
 </body>
+<script>
+    document.querySelector(".menu").onclick = () => {
+    let nav = document.querySelector(".hidden_nav");
+    nav.classList.toggle("open");
+    nav.style.transition = ".5s cubic-bezier(.17,.67,.46,.97)";
+    
+    let menuBurg = document.querySelector(".menu_burguer");
+    menuBurg.classList.toggle("open");
+    menuBurg.style.transition = ".5s cubic-bezier(.17,.67,.46,.97)";
+    };
+</script>
 </html>
