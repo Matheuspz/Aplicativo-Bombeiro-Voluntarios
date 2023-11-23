e68e338c69fec9a03e92f2fd07c10b8b4cdee538
KalfelsWiggers <124676274+KalfelsWiggers@users.noreply.github.com>
2023-11-23 12:09:00 -0300

    Add files via upload

--- bombeiroKalfelsV5/telas/cadastroDoAdmin.php
+++ bombeiroKalfelsV5/telas/cadastroDoAdmin.php
@@ -24,7 +24,31 @@
             right: 0;
             bottom: -15px;
         }
-        
+        .azul-claro{
+            height: 580px;
+            top: 10px;
+        }
+        .adminRadio{
+            width: 100px;
+            position: absolute;
+            right: 220px;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 2px;
+        }
+        .adminOption{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 10px;
+        }
+        @media screen and (max-width: 640px){
+            .adminRadio{
+                right: 161px;
+            }
+
+        }
     </style>
 </head>
 <body>
@@ -34,6 +58,8 @@
             <h1>CADASTRO</h1>
             <form action="">
                 <div class="form-column">
+                    <input type="number" name="" id="" placeholder="Digite um ID">
+                    <hr>
                     <input type="text" name="" id="" placeholder="Digite seu email">
                     <hr>
                     <div class="row">
@@ -51,8 +77,21 @@
                     <input type="submit" value="EXCLUIR" class="btn">
                     <input type="submit" value="ATUALIZAR" class="btn">
 
-                    <a href="telaInicialAdmin.php">
-                        <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+                    <a>
+                        <div class="adminRadio">
+                            <h2>É ADMIN?</h2>
+                            <div class="adminOption">
+                                <label for="adminSim">Sim</label>
+                                <input type="radio" name="opcaoAdmin" id="adminSim">
+                            </div>
+                            <div class="adminOption">
+                                <label for="adminNao">Não</label>
+                                <input type="radio" name="opcaoAdmin" id="adminNao">
+                            </div>
+                        </div>
+                        <a href="telaInicialAdmin.php">
+                            <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+                        </a>
                     </a>
                 </div>
             </form>
