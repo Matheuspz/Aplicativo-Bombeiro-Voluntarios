4da520f93f5f95327f23d7791989239f4b428277
Matheus Pesch Zenere <93321651+Matheuspz@users.noreply.github.com>
2024-08-09 20:02:55 -0300

    Delete Bombeiro PHP 80% pronto directory

--- Bombeiro PHP 80% pronto/js/telaInicial.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".menu").onclick = () => {
-    let nav = document.querySelector(".hidden_nav");
-    nav.classList.toggle("open");
-    nav.style.transition = ".5s cubic-bezier(.17,.67,.46,.97)";
-    
-    let menuBurg = document.querySelector(".menu_burguer");
-    menuBurg.classList.toggle("open");
-    menuBurg.style.transition = ".5s cubic-bezier(.17,.67,.46,.97)";
-    };
